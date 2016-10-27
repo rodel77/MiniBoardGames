0434ed98e8e994cff3449a6864bdaa1a14cc621b
rodel77 <rodelbusiness@hotmail.com>
2016-10-27 02:35:56 -0500

    Updated!

--- example/WhackAMoleExample.java
+++ example/WhackAMoleExample.java
@@ -1,6 +1,7 @@
 package mx.com.rodel.games.defaultgames;
 
 import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -27,37 +28,36 @@ import mx.com.rodel.utils.TimerAPI;
  * 
  * @author rodel77
  */
-public class WhackAMoleExample extends Game{
-	//Initialize Timer
+public class WhackAMole extends Game{
+	//Initialize the Timer API
 	private TimerAPI timer = new TimerAPI(60000);
-	//Slots for mobs spawn
+	//Slots for skull spawning
 	private List<Integer> slots = new ArrayList<>();
-	//Current player points (You can make it like local variable because is singleplayer game)
+	//Current player points (If you are making multiplayer boardgames make this with HashMaps/ArraysLists)
 	private int points = 0;
-	//Current player playing (You can make it like local variable because is singleplayer game)
+	//Player (If you are making multiplayer boardgames make this with HashMaps/ArraysLists)
 	private UUID player;
 	
-	//Initialize constructor, use it for non-inventory/player/bukkit actions
-	public WhackAMoleExample() {
-		//Define slots for mobs spawn
+	//Constructor game, don't use it for Game API methods, because this method go to initialize on load the game and play it
+	public WhackAMole() {
+		//Define skull spawns slots
 		slots.add(10);
 		slots.add(12);
 		slots.add(14);
 		slots.add(16);
 	}
-
-	//Handle game start event
+	
+	//Handle the game start
 	@Override
 	public void onGameStart() {
-		//Set black glass in not special slots
+		//Add black glass in not spawn slots
 		for (int i = 0; i < getGUISize(); i++) {
-			//Slot 22 is for the clock
 			if(!slots.contains(i) && i!=22){
 				sendToAllClients(getCover(), i);
 			}
 		}
-		
-		//The the player variable
+
+		//Define the previous see "player" var
 		for(Entry<UUID, Inventory> p : players.entrySet()){
 			player = p.getKey();
 		}
@@ -69,11 +69,11 @@ public class WhackAMoleExample extends Game{
 	//Handle click event
 	@Override
 	public void onPlayerClickOnGUI(InventoryClickEvent e) {
-		//Check if is mob
-		if(e.getCurrentItem().getType()==Material.SKULL_ITEM && e.getCurrentItem().getItemMeta().getDisplayName().equals(getStringNode("smash")) && slots.contains(e.getSlot())){
-			//Add points
+		//Check if is a game skull
+		if(e.getCurrentItem()!=null && e.getCurrentItem().getType()==Material.SKULL_ITEM && e.getCurrentItem().getItemMeta().getDisplayName().equals(getStringNode("smash")) && slots.contains(e.getSlot())){
+			//Increment points
 			points += 5;
-			//Play sound according the head
+			//Play the sound according the skull
 			switch (e.getCurrentItem().getDurability()) {
 				case 0:
 					SoundHelper.ENTITY_SKELETON_DEATH.play((Player) e.getWhoClicked(), 1, 1);
@@ -84,105 +84,116 @@ public class WhackAMoleExample extends Game{
 				case 2:
 					SoundHelper.ENTITY_ZOMBIE_DEATH.play((Player) e.getWhoClicked(), 1, 1);
 					break;
+				case 3:
+					SoundHelper.ENTITY_ZOMBIE_INFECT.play((Player) e.getWhoClicked(), 1, 1);
+					break;
 				case 4:
 					SoundHelper.ENTITY_CREEPER_DEATH.play((Player) e.getWhoClicked(), 1, 1);
 					break;
 			}
-			//Remove item
 			removeItem(player, e.getSlot());
-			//Increment stat for database
-			incrementStat(player, "mobsKilled", 1);
+			//Increment local stat for send to DB when the game end
+			incrementStat(player, "mobsSmashed", 1);
 			incrementStat(player, "coins", 5);
 		}
 		e.setCancelled(true);
 	}
 	
+	//Handle loop
 	@Override
 	public void loop() {
-		//Set the clock
+		//Set the timer item provided by TimerAPI
 		sendToAllClients(timer.getItemTimer(), 22);
 		
-		//Clear all spawn slots
+		//If random remove all skulls
 		if(new Random().nextInt(10)==0){
 			for(int slot : slots){
 				removeItem(player, slot);
 			}
 		}
 		
-		//Spawn new head
+		//If random spawn new skull
 		if(new Random().nextInt(5)==0){
-			//Remove all slots
+			//Remove all skulls
 			for(int slot : slots){
 				removeItem(player, slot);
 			}
 			
-			//Generate/Put head
+			//Set random spawn and put the skull
 			
 			int[] r = new int[] {0,1,2,4};
 			
-			sendToAllClients(createItem(Material.SKULL_ITEM, r[new Random().nextInt(r.length)], 1, getStringNode("smash")), slots.get(new Random().nextInt(slots.size())));
+			boolean halloween = Helper.getMonth()==9;
+			if(halloween){
+				//This is sooooo spoky...
+				sendToAllClients(Helper.headizer("MHF_Pumpkin", createItem(Material.SKULL_ITEM, 3, 1, getStringNode("smash"))), slots.get(new Random().nextInt(slots.size())));
+			}else{
+				sendToAllClients(createItem(Material.SKULL_ITEM, r[new Random().nextInt(r.length)], 1, getStringNode("smash")), slots.get(new Random().nextInt(slots.size())));
+			}
+			
 		}
 		
-		//End if the timer end
+		//If timer is finished end the game
 		if(timer.isFinished()){
 			end();
 		}
 		
-		//Update coins counter
-		sendToAllClients(createItem(Material.GOLD_NUGGET, 0, points>64 ? 64 : points, getStringNode("points", "{points}:"+points)), 4);
+		//Display the points
+		sendToAllClients(createItem(Material.GOLD_NUGGET, 0, Math.min(points, 64), getStringNode("points", "{points}:"+points)), 4);
 	}
 	
-	//End the game
 	public void end(){
-		//Call end
+		//End the game, true for register stats, false for end without register stats (This is for miss points if you cancel the game)
 		endGame(true);
-		//The the points message
+		//Execute the final command, this is the rewards, you can set it in configuration
+		executeFinalCommand(Bukkit.getPlayer(player));
+		//If is active broadcast send message to all players with player points
 		if(getConfigNode("broadcast", Boolean.class)){
 			sendChatBroadcast(getStringNode("broadcastmsg", "{player}:"+Bukkit.getPlayer(player).getName(), "{points}:"+points));
 		}else{
 			sendChatMessageAll(getStringNode("broadcastmsg", "{player}:"+Bukkit.getPlayer(player).getName(), "{points}:"+points));
 		}
-		//Spawn fireworks
+		//Spawn 5 fireworks 1 each 5 ticks 
 		Helper.spawnFirework(Bukkit.getPlayer(player).getLocation(), 5);
 	}
 	
-	//Setup name
+	//The raw name game, this can be changed in configuration "displayName"
 	@Override
 	public String getName() {
 		return "WhackAMole";
 	}
-	
-	//Setup minimum players
+
+	//Minimum players can join in the game
 	@Override
 	public int getMinPlayers() {
 		return 1;
 	}
-	
-	//Setup maximum players (1 for singleplayer)
+
+	//Maximum players can join in the game (1 for singleplayer games)
 	@Override
 	public int getMaxPlayers() {
 		return 1;
 	}
 
-	//Setup icon
+	//Game icon in game list
 	@Override
 	public Material getIcon() {
 		return Material.DIAMOND_PICKAXE;
 	}
 
-	//Setup minigamecolor
+	//Game color for GUI, this can be changed in configuration "displayName"
 	@Override
 	public ChatColor getMinigameNameColor() {
 		return ChatColor.DARK_GREEN;
 	}
 	
-	//Setup gui size to small
+	//Set GUI size default: BIG_GUI (54)
 	@Override
 	public int getGUISize() {
 		return SMALL_GUI;
 	}
 	
-	//Setup nodes
+	//Register configuration nodes, add here and then when the game load it go to be added in config.yml
 	@Override
 	public HashMap<String, Object> registerConfigNodes(HashMap<String, Object> nodes) {
 		nodes.put("smash", "&aSmash!");
@@ -192,10 +203,11 @@ public class WhackAMoleExample extends Game{
 		return nodes;
 	}
 	
-	//Setup nodes
+	//Register database nodes, add here and then when the game load it go to be added in db
 	@Override
 	public LinkedHashMap<String, String> registerDBStats(LinkedHashMap<String, String> stats) {
-		stats.put("mobsKilled", "INT(10)");
+		//rawName - dbType
+		stats.put("mobsSmashed", "INT(10)");
 		stats.put("coins", "INT(10)");
 		return stats;
 	}
